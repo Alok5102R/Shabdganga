f447171ce3b7ad89e8aefc3020a6baa8f676f571
Ishwar korde <ishwayk@gmail.com>
2024-03-03 11:26:29 +0530

    fixed errors

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,46 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import Home from './pages/Home';
-import { useState,useEffect } from 'react';
-
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-        const [data,setData] = useState([])
-        async function fetchData() {
-                try {
-                    const res = await fetch('http://127.0.0.1:8000/books/api/userapi/');
-                    const apiData = await res.json(); 
-                    setData(apiData);
-                } catch (err) {
-                    console.log(err);
-                }
-            }
-        
-            useEffect(() => {
-                fetchData();
-            }, []); 
-        
-            useEffect(() => {
-                console.log(data); 
-            }, [data]); 
-
-       const username =  data.map((item) => 
-            <div> {item.username} </div>
-            )
-
-
+  // const Modal = () => {
+  //   setIsModalOpen(true ?  isModalOpen : false);
+  // };
 
   return (
     <div>
+      {isModalOpen && (
+       <div>
+       <div className='modal'>
+          <form className='flex flex-col gap-12 '>
+            <h1 className='text-center text-4xl font-serif'>Sign in</h1>
+            <div className='justify-center items-center text-black flex flex-col  text-center'>
+              <input type='text' className='mb-4 px-4 py-3 w-[300px] outline-none rounded focus:border-b-2 focus:border-blue-700' placeholder="Username" /><br />
+              <input type='password' className='mb-4 px-4 py-3  w-[300px] outline-none rounded focus:border-b-2 focus:border-blue-700' placeholder="Password" /><br />
+              <button type='submit' onClick={()=>setIsModalOpen(false)} className='p-2 w-[100px] text-white bg-blue-900 hover:bg-blue-800 rounded'>Submit</button>
+            </div>
+          </form>
+        </div>
+        <div className='overlay' onClick={()=>setIsModalOpen(false)}></div>
+       </div>
         
-        <Nav username={username}/>
+      )
+      }
       
-        <Home/>
+      
+
+      <Nav setIsModalOpen={setIsModalOpen} />
+
+      <Home />
     </div>
-     
-    
-  )
+  );
 }
 
 export default App;
