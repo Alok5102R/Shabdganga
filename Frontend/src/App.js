22538575cb9159f2ba4d1b5608a9f99d06dc43d8
Ishwar korde <ishwayk@gmail.com>
2024-03-09 16:26:08 +0530

    API methods

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Nav from './components/Nav';
 import Home from './pages/Home';
-import { Route,Routes,} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import UploadBook from './pages/UploadBook';
 import UserProfile from './pages/UserProfile';
@@ -9,82 +9,75 @@ import FAQ from './pages/FAQ';
 import ManageBooks from './pages/ManageBooks';
 import ViewBook from './pages/ViewBook';
 
-
-
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [data, setData] = useState([]);
+  const [userData, setUserData] = useState(null);
 
-      async function fetchData() {
-        try {
-          const res = await fetch('http://127.0.0.1:8000/books/api/userprofileapi/	');
-          const apiData = await res.json();
-          setData(apiData);
+  async function fetchData() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/books/api/userprofileapi/');
+      const apiData = await res.json();
 
-        } catch (err) {
-          console.log(err);
-        }
+      const username = 'ishwarsg';
+      const user = apiData.find(profile => profile.user.username === username);
+      if (user) {
+        setUserData(user);
       }
 
-      useEffect(() => {
-        fetchData();
-      }, []); 
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-      useEffect(() => {
-        console.log(data); 
-      }, [data]); 
+  useEffect(() => {
+    fetchData();
+  }, []); 
 
-      const username = data.map((item)=> item.user.username)
-      const fullname = data.map((item)=> item.fullName)
-      const email = data.map((item)=> item.user.email)
-      const country = data.map((item)=>item.country)
-      const gender = data.map((item)=>item.gender)
-      const date = data.map((item)=>item.user.date_joined)
-      const actype = data.map((item)=>item.accountType)
+  useEffect(() => {
+    if (userData) {
+      const { user, fullName, country, gender, accountType } = userData;
+      console.log(user.username);
+      console.log(user.email);
+      console.log(fullName);
+      console.log(country);
+      console.log(gender);
+      console.log(user.date_joined);
+      console.log(accountType);
+    }
+  }, [userData]);
 
-
-
-
-  
   return (
     <div>
+      {/* Conditional rendering of Modal */}
       {isModalOpen && (
-       <div>
-       <div className='modal'>
-          <form className='flex flex-col gap-12 '>
-            <h1 className='text-center text-4xl font-serif'>Sign in</h1>
-            <div className='justify-center items-center text-black flex flex-col  text-center'>
-              <input type='text' className='mb-4 px-4 py-3 w-[300px] outline-none rounded focus:border-b-2 focus:border-blue-700' placeholder="Username" /><br />
-              <input type='password' className='mb-4 px-4 py-3  w-[300px] outline-none rounded focus:border-b-2 focus:border-blue-700' placeholder="Password" /><br />
-              <button type='submit' onClick={()=>setIsModalOpen(false)} className='p-2 w-[100px] text-white bg-blue-900 hover:bg-blue-800 rounded'>Submit</button>
-            </div>
-          </form>
+        <div>
+          <div className='modal'>
+            <form className='flex flex-col gap-12 '>
+              <h1 className='text-center text-4xl font-serif'>Sign in</h1>
+              <div className='justify-center items-center text-black flex flex-col  text-center'>
+                <input type='text' className='mb-4 px-4 py-3 w-[300px] outline-none rounded focus:border-b-2 focus:border-blue-700' placeholder="Username" /><br />
+                <input type='password' className='mb-4 px-4 py-3  w-[300px] outline-none rounded focus:border-b-2 focus:border-blue-700' placeholder="Password" /><br />
+                <button type='submit' onClick={() => setIsModalOpen(false)} className='p-2 w-[100px] text-white bg-blue-900 hover:bg-blue-800 rounded'>Submit</button>
+              </div>
+            </form>
+          </div>
+          <div className='overlay' onClick={() => setIsModalOpen(false)}></div>
         </div>
-        <div className='overlay' onClick={()=>setIsModalOpen(false)}></div>
-       </div>
-        
-      )
-      }
-      
-      
-        {/* NAVBAR buttons */}
+      )}
 
-      <Nav setIsModalOpen={setIsModalOpen} username={username} />
-
+      {/* NAVBAR buttons */}
+      <Nav setIsModalOpen={setIsModalOpen} username={userData ? userData.user.username : null} />
 
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/SignUp" element={<SignUp/>} />
-        <Route path="/UploadBooks" element={<UploadBook/>} />
-        <Route path="/UserProfile" element={<UserProfile username={username} email={email} gender={gender}  date={date} actype={actype} fullname={fullname} country={country}/>} />
-        <Route path="/FAQ" element={<FAQ/>} />
-        <Route path="/UploadBook" element={<UploadBook/>} />
-        <Route path="/ManageBooks" element={<ManageBooks/>} />
-        <Route path="/ViewBook" element={<ViewBook/>} />
-
-    </Routes>
-  
-     
+        <Route path="/" element={<Home />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/UploadBooks" element={<UploadBook />} />
+        <Route path="/UserProfile" element={<UserProfile username={userData ? userData.user.username : null} email={userData ? userData.user.email : null} gender={userData ? userData.gender : null} date={userData ? userData.user.date_joined : null} actype={userData ? userData.accountType : null} fullname={userData ? userData.fullName : null} country={userData ? userData.country : null} />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/UploadBook" element={<UploadBook />} />
+        <Route path="/ManageBooks" element={<ManageBooks />} />
+        <Route path="/ViewBook" element={<ViewBook />} />
+      </Routes>
     </div>
   );
 }
--- Frontend/src/pages/SignUp.js
+++ Frontend/src/pages/SignUp.js
@@ -1,68 +1,113 @@
-import React from 'react'
-import img from '../images/img-signup.png'
+import React, { useState } from 'react';
+import img from '../images/img-signup.png';
 
 function SignUp() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [csrfToken, setCsrfToken] = useState(''); // Assuming you have CSRF token available
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (confirmPassword !== '') {
+      setPasswordMatch(e.target.value === confirmPassword);
+    }
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    setPasswordMatch(password === e.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      username: e.target.username.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+      userType: document.querySelector('input[name="userType"]:checked').value
+    };
+
+    // Example: sending data using fetch with CSRF token
+    try {
+      const response = await fetch('http://127.0.0.1:8000/signup', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrfToken,
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Success - do something here (e.g., redirect to a success page)
+        window.alert('Signed up');
+        console.log('Signup successful');
+        // Optionally, you can reset the form fields here
+      } else {
+        // Handle errors here
+        console.error('Signup failed');
+        window.alert('Signed up failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      
+    }
+  };
+
   return (
-    <div className='flex flex-row  justify-between items-center bg-gray-200'>
-        {/* left side --> Form */}
+    <div className='flex flex-row justify-between items-center bg-gray-200'>
+      {/* left side --> Form */}
+      <div className='relative w-[30%] p-6 m-20 mt-12'>
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-5xl font-semibold font-serif mb-20">Sign Up</h2>
+          <div className="mb-4 flex gap-6">
+            <div>
+              <input type="radio" id="author" name="userType" required value="author"/>
+              <label htmlFor="author">Author</label>
+            </div>
+            <div>
+              <input type="radio" id="reader" name="userType" required value="reader"/>
+              <label htmlFor="reader">Reader</label>
+            </div>
+          </div>
+          <div className="mb-4">
+            <input type="text" id="username" name="username" className="border-gray-300 border-b-4 rounded-md w-full px-3 py-4 focus:border-b-4 focus:border-blue-500 outline-none" placeholder="Username" required/>
+          </div>
+          <div className="mb-4">
+            <input type="email" id="email" name="email" className="border-gray-300 border-b-4 rounded-md w-full px-4 py-3 focus:border-b-4 focus:border-blue-500 outline-none" placeholder="Email" required/>
+          </div>
+          <div className="mb-4">
+            <div className="relative">
+              <input type={showPassword ? 'text' : 'password'} id="password" name="password" value={password} onChange={handlePasswordChange} className="border-gray-300 border-b-4 rounded-md w-full px-4 py-3 focus:border-b-4 focus:border-blue-500 outline-none" placeholder="Password" required minLength={8}/>
+              <button type="button" onClick={togglePasswordVisibility} className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-3">
+                {showPassword ? <i className="far fa-eye"></i> : <i className="far fa-eye-slash"></i>}
+              </button>
+            </div>
+          </div>
+          <div className="mb-4">
+            <input type={showPassword ? 'text' : 'password'} id="cpassword" name="cpassword" value={confirmPassword} onChange={handleConfirmPasswordChange} className="border-gray-300 border-b-4 rounded-md w-full px-4 py-3 focus:border-b-4 focus:border-blue-500 outline-none" placeholder="Confirm Password" required minLength={8}/>
+          </div>
+          {!passwordMatch && confirmPassword !== '' && <p className="text-red-500">Passwords do not match</p>}
+          <div className="mb-4">
+            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors">Sign Up</button>
+          </div>
+        </form>
+      </div>
 
-        <div className='relative w-[30%] p-6 m-20 mt-12'>
-                <form action="#" method="POST">
-                <h2 className="text-5xl  font-semibold font-serif mb-20">Sign Up</h2>
-
-
-                <div class="mb-4 flex gap-6"> 
-
-                    <div>
-                    <input type="radio" id="author" name="userType" value="author"/>
-                    <label for="author">Author</label>
-                    </div>
-
-                    <div>
-                    <input type="radio" id="reader" name="userType" value="reader"/>
-                    <label for="reader">Reader</label>
-                    </div>
-
-                </div>
-
-                    <div className="mb-4">
-                        <input type="text" id="username" name="username" className="border-gray-300 border-b-4 rounded-md w-full px-3 py-4 focus:border-b-4 focus:border-blue-500 outline-none"  placeholder="Username" />
-                    </div>
-                    <div className="mb-4">
-                       
-                        <input type="text" id="email" name="email" className="border-gray-300 border-b-4 rounded-md w-full px-4 py-3 focus:border-b-4 focus:border-blue-500 outline-none"  placeholder="Email" />                    
-                    </div>
-
-
-
-
-                    <div className="mb-4">
-                        
-                    <input type="text" id="password" name="password" className="border-gray-300 border-b-4 rounded-md w-full px-4 py-3 focus:border-b-4 focus:border-blue-500 outline-none"  placeholder="Password" />                    
-                    </div>
-                            
-                    <div className="mb-4">
-                    <input type="text" id="cpassword" name="cpassword" className="border-gray-300 border-b-4 rounded-md w-full px-4 py-3 focus:border-b-4 focus:border-blue-500 outline-none"  placeholder="Confirm Password" />                    
-                    </div>
-                    <div className="mb-4">
-                        <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors">Sign Up</button>
-                    </div>
-                </form>
-        </div>
-
-                <div className='border w-0 h-80 border-gray-300'>
-                        
-                </div>
-
-
-        {/* right side --> Image*/}
-        <div className='relative  m-20'>
-            <img src={img} alt=""
-                className='h-[500px] w-[500px]'
-            />
-        </div>
+      <div className='border w-0 h-80 border-gray-300'></div>
+      
+      {/* right side --> Image*/}
+      <div className='relative m-20'>
+        <img src={img} alt="" className='h-[500px] w-[500px]'/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SignUp;
