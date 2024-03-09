e1333af77e20b49e224dddc8ed871ab21c768b0a
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-03-09 13:30:58 +0530

    sign in view added and script run update

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = DjangoKey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- startapp.bat
+++ startapp.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start "Script 1" cmd /k "cd Frontend && npm start"
-start "Script 2" cmd /k "cd Backend/backend && python manage.py runserver"
+start "NodeJS" cmd /k "cd Frontend && npm start"
+start "DjangoPy" cmd /k "cd Backend/backend && python manage.py runserver"
