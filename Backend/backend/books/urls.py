89c5446a4f556566c50e74fe6600064103ebefdd
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-03-27 23:36:53 +0530

    basename updated for apis

--- Backend/backend/books/urls.py
+++ Backend/backend/books/urls.py
@@ -4,14 +4,14 @@ from rest_framework import routers
 
 booksrouter = routers.DefaultRouter()
 
-booksrouter.register(r'userapi',views.UserViewset)
+booksrouter.register(r'userapi',views.UserViewset,basename='user')
 booksrouter.register(r'profileapi',views.ProfileViewset,basename='profile')
 booksrouter.register(r'userprofileapi',views.UserProfileViewset,basename='userprofile')
-booksrouter.register(r'bookapi',views.BookViewset)
-booksrouter.register(r'authorapi',views.AuthorViewset)
-booksrouter.register(r'languageapi',views.LanguageViewset)
-booksrouter.register(r'genreapi',views.GenreViewset)
-booksrouter.register(r'signinapi',views.SigninViewset)
+booksrouter.register(r'bookapi',views.BookViewset,basename='book')
+booksrouter.register(r'authorapi',views.AuthorViewset,basename='author')
+booksrouter.register(r'languageapi',views.LanguageViewset,basename='language')
+booksrouter.register(r'genreapi',views.GenreViewset,basename='genere')
+booksrouter.register(r'signinapi',views.SigninViewset,basename='signin')
 
 
 
@@ -20,9 +20,9 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('whoami',views.whoami,name='whoami'),
     path('csrf-token/', views.csrf_token_view, name='csrf_token'),
-    path('login',views.LoginView.as_view()),
-    path('user',views.UserView.as_view()),
-    path('userdetail',views.LoggedInUser.as_view()),
+    path('login',views.LoginView.as_view(),name='login'),
+    path('user',views.UserView.as_view(),name='loggeduser'),
+    path('userdetail',views.LoggedInUser.as_view(),name='loggeduserdetails'),
 
 
 
